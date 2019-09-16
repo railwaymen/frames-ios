5589526257cbd3b0a35c1a5c98d3b116f60181f3
Piotr Pawluś <piotr.pawlus@railwaymen.org>
2019-09-16 14:00:57 +0200

    Parse all apple pay token values

--- Source/ApplePayToken.swift
+++ Source/ApplePayToken.swift
@@ -3,6 +3,18 @@ import Foundation
 /// Apple Pay token
 public struct ApplePayToken: Codable {
 
+    /// Bin of the apple pay
+    public let bin: String
+
+    /// Credit card expiry year
+    public let expiryYear: Int
+
+    /// Credit card expiry month
+    public let expiryMonth: Int
+
+    /// Credit card last four numbers
+    public let last4: String
+
     /// Type of the token: applepay
     public let type: String
 
